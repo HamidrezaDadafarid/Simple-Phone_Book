bcb88ff47e4a15e27ba6db912e9804df0cebbfd4
Hamidreza Dadafarid <148267814+HamidrezaDadafarid@users.noreply.github.com>
2024-08-13 00:50:48 +0330

    modalized getting input from user in main

--- main.py
+++ main.py
@@ -2,103 +2,109 @@ from phonebook import User, PhoneBook
 
 
 def print_main_menu():
-    return "****************************************\n1- Add new user\n2- Edit an existing user\n3- Delete a user based on their name\n4- Show all users\n5- Sort the users"
+    return (
+        "****************************************\n"
+        "1- Add new user\n"
+        "2- Edit an existing user\n"
+        "3- Delete a user based on their name\n"
+        "4- Show all users\n"
+        "5- Sort the users"
+    )
 
 
 def print_edit_menu():
-    return "****************************************\n1- Edit user's name\n2- Edit user's phone number\n3- edit user's email"
+    return (
+        "****************************************\n"
+        "1- Edit user's name\n"
+        "2- Edit user's phone number\n"
+        "3- edit user's email"
+    )
+
+
+def get_user_choice(prompt):
+    try:
+        return int(input(prompt))
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
+        return None
 
 
 def main():
     phone_book = PhoneBook()
     while True:
         print(print_main_menu())
-        try:
-            n = int(
-                input("\nPlease enter the number of your choice (enter 0 to exit): "))
-        except ValueError:
-            print("Invalid input! Please enter a valid number.")
-        except KeyboardInterrupt:
-            print("\nKeyboard interrupt detected. Exiting the menu. Goodbye!")
-            break
-        else:
-            if n == 0:
-                print("Exiting the menu. Goodbye!")
-                break
-            elif n == 1:
-                returned_value = phone_book.get_inputs_and_validate_them_for_adding_user()
-                if isinstance(returned_value, User):
-                    if returned_value.name in phone_book.users:
-                        print("User already in phonebook!")
-                        continue
-                    print(phone_book.add_user(returned_value))
-                elif not returned_value:
-                    continue
-                else:
-                    print("\nKeyboard interrupt detected. Exiting the menu. Goodbye!")
-                    break
-            elif n == 2:
-                print(print_edit_menu())
-                try:
-                    n = int(
-                        input(
-                            "\nPlease enter a number to edit the user's information: ")
-                    )
-                except ValueError:
-                    print("Invalid input! Please enter a valid number.")
-                except KeyboardInterrupt:
-                    print("\nKeyboard interrupt detected. Exiting the menu. Goodbye!")
-                    break
-                else:
-                    try:
-                        if n in [1, 2, 3]:
-                            name = input(
-                                "Please enter the name of the user that you want to edit: ")
-                            if phone_book.validate_name(name) != True:
-                                print(phone_book.validate_name(name))
-                                continue
-                            elif name not in phone_book.users:
-                                print("User not found!")
-                                continue
-                            if n == 1:
-                                new_name = input(
-                                    "Please enter the new name of your user: ")
-                                print(phone_book.edit_name(name, new_name))
-                            elif n == 2:
-                                new_phone_number = input(
-                                    "Please enter the new phone number of your user: ")
-                                print(phone_book.edit_phone_number(
-                                    name, new_phone_number))
-                            elif n == 3:
-                                new_email = input(
-                                    "Please enter the new email of your user: ")
-                                print(phone_book.edit_email(name, new_email))
-                        else:
-                            print("Please select a valid option between 1 and 3.")
-                            continue
-                    except KeyboardInterrupt:
-                        print(
-                            "\nKeyboard interrupt detected. Exiting the menu. Goodbye!")
-                        break
+        n = get_user_choice(
+            "\nPlease enter the number of your choice (enter 0 to exit): ")
 
-            elif n == 3:
+        if n is None:
+            continue
+
+        if n == 0:
+            print("Exiting the menu. Goodbye!")
+            break
+
+        elif n == 1:
+            returned_value = phone_book.get_inputs_and_validate_them_for_adding_user()
+            if isinstance(returned_value, User):
+                if returned_value.name in phone_book.users:
+                    print("User already in phonebook!")
+                    continue
+                print(phone_book.add_user(returned_value))
+            elif returned_value is None:
+                continue
+
+        elif n == 2:
+            print(print_edit_menu())
+            edit_choice = get_user_choice(
+                "\nPlease enter a number to edit the user's information: ")
+            if edit_choice is None:
+                continue
+            if edit_choice in [1, 2, 3]:
                 name = input(
-                    "Please enter the user's name you want to delete: ")
-                if phone_book.validate_name(name) != True:
+                    "Please enter the name of the user that you want to edit: ")
+                if phone_book.validate_name(name) is not True:
                     print(phone_book.validate_name(name))
                     continue
                 elif name not in phone_book.users:
                     print("User not found!")
                     continue
-                else:
-                    print(phone_book.delete_user(name))
-            elif n == 4:
-                phone_book.show_all_users()
-            elif n == 5:
-                print(phone_book.sort_users())
+                if edit_choice == 1:
+                    new_name = input(
+                        "Please enter the new name of your user: ")
+                    print(phone_book.edit_name(name, new_name))
+                elif edit_choice == 2:
+                    new_phone_number = input(
+                        "Please enter the new phone number of your user: ")
+                    print(phone_book.edit_phone_number(
+                        name, new_phone_number))
+                elif edit_choice == 3:
+                    new_email = input(
+                        "Please enter the new email of your user: ")
+                    print(phone_book.edit_email(name, new_email))
             else:
-                print("Please select a valid option between 1 and 5.")
+                print("Please select a valid option between 1 and 3.")
                 continue
+
+        elif n == 3:
+            name = input(
+                "Please enter the user's name you want to delete: ")
+            if phone_book.validate_name(name) is not True:
+                print(phone_book.validate_name(name))
+                continue
+            elif name not in phone_book.users:
+                print("User not found!")
+                continue
+            else:
+                print(phone_book.delete_user(name))
+
+        elif n == 4:
+            phone_book.show_all_users()
+
+        elif n == 5:
+            print(phone_book.sort_users())
+        else:
+            print("Please select a valid option between 1 and 5.")
+            continue
 
 
 if __name__ == "__main__":
--- phonebook.py
+++ phonebook.py
@@ -20,23 +20,18 @@ class PhoneBook:
     def validate_name(self, name: str) -> Union[str, bool]:
         if not name:
             return "User's name should not be empty!"
-        if len(name) < 1 or len(name) > 50:
+        if not (1 <= len(name) <= 50):
             return "User's name length must be from 1 to 50 characters!"
         pattern = re.compile(r"^[A-Za-z0-9\s'-]+$")
         if not pattern.match(name):
             return "User's name has some invalid characters. it should only contain a-z, A-Z, whitespaces and - characters!"
-
         return True
 
     def validate_phone_number(self, phone_number: str) -> Union[str, bool]:
         if not phone_number:
             return "User's phone number should not be empty!"
-        if not phone_number.isdigit():
-            return "User's phone number must only be digits!"
-        pattern = re.compile(r"^09\d{9}$")
-        if not pattern.match(phone_number):
-            return "User's phone number must be 11 digits. (09...)"
-
+        if not phone_number.isdigit() or not re.match(r"^09\d{9}$", phone_number):
+            return "User's phone number must be 11 digits and only contain digits!"
         return True
 
     def validate_email(self, email: str) -> Union[str, bool]:
@@ -46,26 +41,24 @@ class PhoneBook:
             r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
         if not pattern.match(email):
             return "User's email is invalid!"
-
         return True
 
     def get_inputs_and_validate_them_for_adding_user(self) -> Union[User, KeyboardInterrupt, None]:
         try:
             name = input("Please enter your user's name: ")
-            if self.validate_name(name) != True:
+            if self.validate_name(name) is not True:
                 print(self.validate_name(name))
                 return None
             phone_number = input("Please enter your user's phone number: ")
-            if self.validate_phone_number(phone_number) != True:
+            if self.validate_phone_number(phone_number) is not True:
                 print(self.validate_phone_number(phone_number))
                 return None
             email = input("Please enter your user's email: ")
-            if self.validate_email(email) != True:
+            if self.validate_email(email) is not True:
                 print(self.validate_email(email))
                 return None
 
             return User(name, phone_number, email)
-
         except KeyboardInterrupt:
             return KeyboardInterrupt
 
@@ -78,21 +71,21 @@ class PhoneBook:
         return "User added successfully!"
 
     def edit_name(self, name: str, new_name: str) -> str:
-        if self.validate_name(new_name) == True:
+        if self.validate_name(new_name) is True:
             self.users[new_name] = self.users.pop(name)
             self.save_users_to_file()
             return "User's name edited successfully!"
         return self.validate_name(new_name)
 
     def edit_phone_number(self, name: str, new_phone_number: str) -> str:
-        if self.validate_phone_number(new_phone_number) == True:
+        if self.validate_phone_number(new_phone_number) is True:
             self.users[name]["phone_number"] = new_phone_number
             self.save_users_to_file()
             return "User's phone number edited successfully!"
         return self.validate_phone_number(new_phone_number)
 
     def edit_email(self, name: str, new_email: str) -> str:
-        if self.validate_email(new_email) == True:
+        if self.validate_email(new_email) is True:
             self.users[name]["email"] = new_email
             self.save_users_to_file()
             return "User's email edited successfully!"
